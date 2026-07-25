76c73cf778a3930437df0ed6216fe4bd74f02e4f
ammaryasser2000 <ammar11amma2019@gmail.com>
2026-07-25 08:26:32 +0300

    Initialize bot configuration in config.py
    Add configuration settings for bot token, admin IDs, subscriptions, name, and languages.

--- config.py
+++ config.py
@@ -1,1 +1,23 @@
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
+
+BOT_TOKEN = os.getenv("8790701693:AAHfsOlGQVqp4zNLsgcs9Racjrk99U3bN2M")
+
+# ضع Telegram ID 8907883947
+ADMIN_IDS = [e459b21995d2c3f98a86368c90a2efe0]
+
+# مدة الاشتراكات بالأيام
+WEEK_SUBSCRIPTION = 7
+MONTH_SUBSCRIPTION = 30
+
+# اسم البوت
+BOT_NAME = "Orange Movements"
+
+# لغات البوت
+LANGUAGES = {
+    "ar": "العربية",
+    "en": "English",
+    "bn": "বাংলা"
+}
