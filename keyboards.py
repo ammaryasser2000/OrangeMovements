08d80a7ae74a876543cd1c0c7ab4f038856e1fb3
ammaryasser2000 <ammar11amma2019@gmail.com>
2026-07-25 14:08:45 +0300

    Add main menu keyboard for Telegram bot

--- keyboards.py
+++ keyboards.py
@@ -1,1 +1,20 @@
+from telegram import ReplyKeyboardMarkup
 
+def main_menu():
+
+    keyboard = [
+
+        ["🔔 المراقبة"],
+
+        ["💎 الاشتراك", "👤 حسابي"],
+
+        ["🌍 تغيير اللغة"],
+
+        ["📞 التواصل مع الإدارة"]
+
+    ]
+
+    return ReplyKeyboardMarkup(
+        keyboard,
+        resize_keyboard=True
+    )
