58f80ab096e5abead1d1dea57e5824e0ed429df0
ammaryasser2000 <ammar11amma2019@gmail.com>
2026-07-25 14:07:02 +0300

    Update database.py

--- database.py
+++ database.py
@@ -1,1 +1,52 @@
+import sqlite3
+from config import DATABASE_NAME
 
+def connect():
+    return sqlite3.connect(DATABASE_NAME)
+
+def create_tables():
+    conn = connect()
+    cur = conn.cursor()
+
+    # جدول المستخدمين
+    cur.execute("""
+    CREATE TABLE IF NOT EXISTS users (
+        user_id INTEGER PRIMARY KEY,
+        username TEXT,
+        full_name TEXT,
+        language TEXT DEFAULT 'ar',
+        subscription TEXT DEFAULT 'none',
+        expire_date TEXT
+    )
+    """)
+
+    conn.commit()
+    conn.close()
+
+def add_user(user_id, username, full_name):
+    conn = connect()
+    cur = conn.cursor()
+
+    cur.execute("""
+    INSERT OR IGNORE INTO users
+    (user_id, username, full_name)
+    VALUES (?, ?, ?)
+    """, (user_id, username, full_name))
+
+    conn.commit()
+    conn.close()
+
+def get_user(user_id):
+    conn = connect()
+    cur = conn.cursor()
+
+    cur.execute(
+        "SELECT * FROM users WHERE user_id=?",
+        (user_id,)
+    )
+
+    user = cur.fetchone()
+
+    conn.close()
+
+    return user
