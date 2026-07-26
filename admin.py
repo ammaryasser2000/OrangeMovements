11918c243c50eed3168fe21cb3c269b0b4d4a31e
ammaryasser2000 <ammar11amma2019@gmail.com>
2026-07-26 14:22:36 +0300

    Implement subscription commands for Telegram bot
    Added command handlers for weekly and monthly subscriptions.

--- admin.py
+++ admin.py
@@ -1,1 +1,54 @@
 
+from telegram import Update
+from telegram.ext import CommandHandler, ContextTypes
+
+from subscriptions import add_subscription
+
+# ضع هنا رقم حسابك في تيليجرام
+ADMIN_ID = 4915568330141
+
+
+async def week(update: Update, context: ContextTypes.DEFAULT_TYPE):
+
+    if update.effective_user.id != ADMIN_ID:4915568330141
+        return
+
+    if len(context.args) != 1:
+        await update.message.reply_text(
+            "استخدم:\n/week USER_ID"
+        )
+        return
+
+    user_id = int(context.args[0])
+
+    add_subscription(user_id, "week")
+
+    await update.message.reply_text(
+        "✅ تم إضافة اشتراك أسبوعي."
+    )
+
+
+async def month(update: Update, context: ContextTypes.DEFAULT_TYPE):
+
+    if update.effective_user.id != ADMIN_ID:
+        return
+
+    if len(context.args) != 1:
+        await update.message.reply_text(
+            "استخدم:\n/month USER_ID"
+        )
+        return
+
+    user_id = int(context.args[0])
+
+    add_subscription(user_id, "month")
+
+    await update.message.reply_text(
+        "✅ تم إضافة اشتراك شهري."
+    )
+
+
+admin_handlers = [
+    CommandHandler("week", week),
+    CommandHandler("month", month),
+]
