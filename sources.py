5346fb65472aeb35551fde2875d14e1155b27d09
ammaryasser2000 <ammar11amma2019@gmail.com>
2026-07-26 14:43:03 +0300

    Replace email and password with hardcoded values
    Hardcoded email and password in OrangeCarrier class.

--- sources.py
+++ sources.py
@@ -59,8 +59,8 @@ class IVAS:
 class OrangeCarrier:
 
     def __init__(self, email, password):
-        self.email = email
-        self.password = password
+        self.email = ammar11ammar2019@gmail.com 
+        self.password = 7700208345Ab$
 
     async def login(self):
 
