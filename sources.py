7033d0def6c55db6317f19a8217bf30bf64327a7
ammaryasser2000 <ammar11amma2019@gmail.com>
2026-07-27 13:10:51 +0300

    Update sources.py

--- sources.py
+++ sources.py
@@ -1,15 +1,14 @@
 from playwright.async_api import async_playwright
 class IVAS:
- def __init__(self, email, password):
-        self.email = email
-        self.password = password
+    def __init__(self, email, password):
+        self.email = ammar11ammar2019@gmail.com 
+        self.password = 7700208345Ab
     async def login(self):
         self.playwright = await async_playwright().start()
-   self.browser = await self.playwright.chromium.launch(
+        self.browser = await self.playwright.chromium.launch(
             headless=True
         )
-       self.page = await self.browser.new_page()
-
+        self.page = await self.browser.new_page()
         await self.page.goto(
             "https://www.ivasms.com/login"
         )
@@ -25,8 +24,9 @@ class IVAS:
             'button[type="submit"]'
         )
         await self.page.wait_for_timeout(5000)
-   async def open_live(self):
-       await self.page.goto(
+
+    async def open_live(self):
+        await self.page.goto(
             "https://www.ivasms.com/portal/live/test_sms"
         )
         await self.page.wait_for_timeout(3000)
@@ -37,10 +37,10 @@ class IVAS:
         await self.playwright.stop()
 class OrangeCarrier:
     def __init__(self, email, password):
-        self.email = email
-        self.password = password
+        self.email = ammar11ammar2019@gmail.com 
+        self.password = 7700208345Ab$
     async def login(self):
-       self.playwright = await async_playwright().start()
+        self.playwright = await async_playwright().start()
         self.browser = await self.playwright.chromium.launch(
             headless=True
         )
@@ -63,6 +63,7 @@ class OrangeCarrier:
     async def dashboard(self):
         html = await self.page.content()
         return html
+
     async def close(self):
         await self.browser.close()
         await self.playwright.stop()
