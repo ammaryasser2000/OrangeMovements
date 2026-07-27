e8ed2828306f444cec8aeb791587a94123543fc5
ammaryasser2000 <ammar11amma2019@gmail.com>
2026-07-27 11:36:43 +0300

    Update keyboards.py

--- keyboards.py
+++ keyboards.py
@@ -1,19 +1,11 @@
 from telegram import ReplyKeyboardMarkup
-
 def main_menu():
-
     keyboard = [
-
         ["🔔 المراقبة"],
-
         ["💎 الاشتراك", "👤 حسابي"],
-
         ["🌍 تغيير اللغة"],
-
         ["📞 التواصل مع الإدارة"]
-
     ]
-
     return ReplyKeyboardMarkup(
         keyboard,
         resize_keyboard=True
