1e5b12e4fc2040b755c513e137bcd917fcd8e00e
ammaryasser2000 <ammar11amma2019@gmail.com>
2026-07-27 11:50:26 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,18 +2,13 @@ import os
 from telegram.ext import ApplicationBuilder
 from database import create_tables, add_user
 from admin import admin_handlers
-# التوكن الخاص بالبوت مباشرة
 TOKEN = "8790701693:AAHfsOlGQVqp4zNLsgcs9Racjrk99U3bN2M"
 def main():
-    # إنشاء الجداول عند البدء
     create_tables()
-    # بناء تطبيق البوت باستخدام التوكن
     application = ApplicationBuilder().token(TOKEN).build()
-    # إضافة أوامر المشرفين المستوردة من ملف admin.py
     for handler in admin_handlers:
         application.add_handler(handler)
-    print("Bot is running...")    
-    # تشغيل البوت
+    print("Bot is running...")
     application.run_polling()
 if __name__ == "__main__":
     main()
