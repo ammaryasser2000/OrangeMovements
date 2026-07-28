5d1a843c9b013973842ff7888316031e420afff0
ammaryasser2000 <ammar11amma2019@gmail.com>
2026-07-28 13:04:13 +0300

    Update config.py

--- config.py
+++ config.py
@@ -2,20 +2,20 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 # Telegram
-BOT_TOKEN = os.getenv("8790701693:AAHfsOlGQVqp4zNLsgcs9Racjrk99U3bN2M")
+BOT_TOKEN = "8790701693:AAHfsOlGQVqp4zNLsgcs9Racjrk99U3bN2M"
 # Database
 DATABASE_NAME = "orange.db"
 # IVAS
-IVAS_EMAIL = os.getenv("ammar11ammar2019@gmail.com")
-IVAS_PASSWORD = os.getenv("770208345Ab")
+IVAS_EMAIL = "ammar11ammar2019@gmail.com"
+IVAS_PASSWORD = "770208345Ab"
 # OrangeCarrier
-ORANGE_EMAIL = os.getenv("ammar11ammar2019@gmail.com")
-ORANGE_PASSWORD = os.getenv("770208345Ab$")
+ORANGE_EMAIL = "ammar11ammar2019@gmail.com"
+ORANGE_PASSWORD = "770208345Ab$"
 # مجموعة الإشعارات
 GROUP_ID = -1003764162114
 # الأدمن
 ADMIN_IDS = [
-    8907883947  # ضع معرفك هنا
+    8907883947
 ]
 SUPPORT = [
     "@b_6_0",
