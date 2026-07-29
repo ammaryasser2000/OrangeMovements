8b2d467baa23cc0e9f4851e61aa2348f6475e9d7
ammaryasser2000 <ammar11amma2019@gmail.com>
2026-07-29 09:06:15 +0300

    Refactor database.py to update tables and add functions

--- database.py
+++ database.py
@@ -1,16 +1,10 @@
 import sqlite3
 from config import DATABASE_NAME
-
-
 def connect():
     return sqlite3.connect(DATABASE_NAME)
-
-
 def create_tables():
     conn = connect()
     cur = conn.cursor()
-
-    # جدول المستخدمين
     cur.execute("""
     CREATE TABLE IF NOT EXISTS users (
         user_id INTEGER PRIMARY KEY,
@@ -21,18 +15,6 @@ def create_tables():
         expire_date TEXT
     )
     """)
-
-    # جدول الحركات
-    cur.execute("""
-    CREATE TABLE IF NOT EXISTS movements (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        country TEXT,
-        number TEXT,
-        created_at TEXT
-    )
-    """)
-
-    # جدول الاشتراكات
     cur.execute("""
     CREATE TABLE IF NOT EXISTS subscriptions (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -43,36 +25,61 @@ def create_tables():
         active INTEGER DEFAULT 1
     )
     """)
-
-    conn.commit()
-    conn.close()
-
-
+    cur.execute("""
+    CREATE TABLE IF NOT EXISTS movements (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        source TEXT,
+        country TEXT,
+        code TEXT,
+        service TEXT,
+        movement_key TEXT UNIQUE,
+        created_at TEXT
+    )
+    """)
+    conn.commit()    conn.close()
 def add_user(user_id, username, full_name):
     conn = connect()
     cur = conn.cursor()
-
     cur.execute("""
     INSERT OR IGNORE INTO users
     (user_id, username, full_name)
     VALUES (?, ?, ?)
     """, (user_id, username, full_name))
-
     conn.commit()
     conn.close()
-
-
 def get_user(user_id):
     conn = connect()
     cur = conn.cursor()
-
-    cur.execute(
+  cur.execute(
         "SELECT * FROM users WHERE user_id=?",
         (user_id,)
     )
-
     user = cur.fetchone()
-
     conn.close()
-
     return user
+def movement_exists(key):
+    conn = connect()
+    cur = conn.cursor()
+    cur.execute(
+        "SELECT id FROM movements WHERE movement_key=?",
+        (key,)
+    )
+    row = cur.fetchone()
+    conn.close()
+    return row is not None
+def save_movement(source, country, code, service, key):
+    conn = connect()
+    cur = conn.cursor()
+    cur.execute("""
+    INSERT OR IGNORE INTO movements
+    (source,country,code,service,movement_key,created_at)
+    VALUES(?,?,?,?,?,datetime('now'))
+    """, (
+        source,
+        country,
+        code,
+        service,
+        key
+    ))
+    conn.commit()
+    conn.close()
