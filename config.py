033759313daae6d572890f2507d07e159582f770
ammaryasser2000 <ammar11amma2019@gmail.com>
2026-07-29 08:22:07 +0300

    Use environment variables for configuration values
    Refactor configuration to use environment variables for sensitive data.

--- config.py
+++ config.py
@@ -2,22 +2,20 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 # Telegram
-BOT_TOKEN = "8790701693:AAHfsOlGQVqp4zNLsgcs9Racjrk99U3bN2M"
+BOT_TOKEN = os.getenv("BOT_TOKEN")
 # Database
 DATABASE_NAME = "orange.db"
 # IVAS
-IVAS_EMAIL = "ammar11ammar2019@gmail.com"
-IVAS_PASSWORD = "770208345Ab"
-# OrangeCarrier
-ORANGE_EMAIL = "ammar11ammar2019@gmail.com"
-ORANGE_PASSWORD = "770208345Ab$"
-# مجموعة الإشعارات
-GROUP_ID = -1003764162114
-# الأدمن
+IVAS_EMAIL = os.getenv("ammar11ammar2019@gmail.com")
+IVAS_PASSWORD = os.getenv("770208345Ab")
+# Orange Carrier
+ORANGE_EMAIL = os.getenv("ammar11ammar2019@gmail.com")
+ORANGE_PASSWORD = os.getenv("770208345Ab$")
+# Telegram Group
+GROUP_ID = int(os.getenv("GROUP_ID", "-1003764162114"))
+# Admin
 ADMIN_IDS = [
-    8907883947
+    int(os.getenv("ADMIN_ID", "8907883947"))
 ]
-SUPPORT = [
-    "@b_6_0",
-    "@b_6_01"
-]
+SUPPORT_USERNAME = "@b_6_01"
+CHECK_INTERVAL = 30
